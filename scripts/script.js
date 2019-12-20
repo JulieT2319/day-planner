72cf049ca1a6082899c4898186984f090da906e8
Julie T <47932789+JulieT2319@users.noreply.github.com>
2019-12-20 11:29:14 -0700

    added getting schedule from local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
+var schedule = JSON.parse(localStorage.getItem(schedule));
+
+if (schedule === null) {
+  schedule = [];
+}
+
 $(".lock").on("click", function() {
   var lock = $(this).attr("data-lock");
-  console.log(lock);
+
   if (lock === "locked") {
     $(this).html("<i class='fas fa-unlock    '></i>");
     $(this).attr("data-lock", "unlocked");
