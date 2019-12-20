b105db13a04a50e3927094ab2188e5fd591b41e6
Julie T <47932789+JulieT2319@users.noreply.github.com>
2019-12-20 12:52:02 -0700

    adjusted functions to properly reference local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-var schedule = JSON.parse(localStorage.getItem(schedule));
-
+var schedule = JSON.parse(localStorage.getItem("schedule"));
+console.log(schedule);
 if (schedule === null) {
   schedule = {
     lockState: [
       "open",
-      "open",
+      "locked",
       "open",
       "open",
       "open",
@@ -13,19 +13,44 @@ if (schedule === null) {
       "open",
       "open"
     ],
-    scheduleTask: [, , , , , , , , ,]
+    scheduleTask: [, "stealth mode", , , , , , , ,],
+    timeSlot: ["am9", "am10", "am11", "pm12", "pm1", "pm2", "pm3", "pm4", "pm5"]
   };
 }
 console.log(schedule);
+for (i = 0; i < schedule.timeSlot.length; i++) {
+  var id = "#" + schedule.timeSlot[i];
+  var id2 = "#lock-" + i;
+  var task = schedule.scheduleTask[i];
+  var lock = schedule.lockState[i];
+  $(id).text(task);
+  $(id2).attr("data-lock", lock);
+  if (lock === "locked") {
+    $(id).attr("disabled", "disabled");
+    $(id2).html("<i class='fas fa-lock    '></i>");
+  }
+}
 $(".lock").on("click", function() {
   var lock = $(this).attr("data-lock");
   var index = $(this).attr("data-index");
+  var id = "#" + schedule.timeSlot[index];
+  var text = $(id).val();
 
   if (lock === "locked") {
     $(this).html("<i class='fas fa-unlock    '></i>");
     $(this).attr("data-lock", "unlocked");
+    schedule.lockState[index] = "unlocked";
+    $(id).removeAttr("disabled");
+    schedule.scheduleTask[index] = null;
   } else {
     $(this).html("<i class='fas fa-lock    '></i>");
     $(this).attr("data-lock", "locked");
+    schedule.lockState[index] = "locked";
+    $(id).attr("disabled", "disabled");
+    schedule.scheduleTask[index] = text;
   }
+  console.log(schedule);
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+  schedule = JSON.parse(localStorage.getItem("schedule"));
+  console.log(schedule);
 });
