0eddcb77fe5e38bfe0cab8f315bfc617b4bf3a44
Julie T <47932789+JulieT2319@users.noreply.github.com>
2019-12-20 11:45:29 -0700

    added data-index attribute

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
 var schedule = JSON.parse(localStorage.getItem(schedule));
 
 if (schedule === null) {
-  schedule = [];
+  schedule = {
+    lockState: [
+      "open",
+      "open",
+      "open",
+      "open",
+      "open",
+      "open",
+      "open",
+      "open",
+      "open"
+    ],
+    scheduleTask: [, , , , , , , , ,]
+  };
 }
-
+console.log(schedule);
 $(".lock").on("click", function() {
   var lock = $(this).attr("data-lock");
+  var index = $(this).attr("data-index");
 
   if (lock === "locked") {
     $(this).html("<i class='fas fa-unlock    '></i>");
